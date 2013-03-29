a7fc8e25f15e0a5411b016f8d5649d05a837f437
ryan earl <earl@veracityins.com>
2013-03-29 16:39:02 -0600

    started on the navigation function

--- ui_javascripts/navigation.js
+++ ui_javascripts/navigation.js
@@ -1,5 +1,10 @@
+var screen_index = 'home_screen';
 
 function navigation(screen_name){
-	console.log(screen_name);
-
+	var new_screen = screen_name.getAttribute('value');
+    if(new_screen !== null){
+        id(screen_index).style.display = 'none';
+        id(new_screen).style.display = 'block';
+    }
+    screen_index = new_screen;
 }
