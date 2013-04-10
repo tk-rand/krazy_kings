f1fbb47546696689e271ccba8142af651d8df7a0
unknown <Ryan@.(none)>
2013-04-09 18:51:07 -0600

    stuff

--- ui_javascripts/main_screen.js
+++ ui_javascripts/main_screen.js
@@ -1,6 +1,11 @@
 
 
 
-function Game(){
+function Game(player_name){
+	var _game = new game();
+	
+	var game_constants = _game.initilize_game(2, player_name);
+	
+	game_constants = _game.start_game(game_constants);
 
 }
