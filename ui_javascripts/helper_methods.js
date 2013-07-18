a63a61327e4cd1675ad417c97d3dd6028ee79b4d
ryan earl <earl@veracityins.com>
2013-07-18 16:35:46 -0600

    work on drawing the screen and also a refactor of the main Game class.

--- ui_javascripts/helper_methods.js
+++ ui_javascripts/helper_methods.js
@@ -7,3 +7,54 @@ function id(ele_id){
 function cls(class_name){
     return document.getElementsByClassName(class_name);
 }
+
+function get_number_of_players_and_player_names(){
+    var num_of_players =  prompt("Enter number of players desired (up to 4).");
+    var name_of_players = [];
+
+    for(var i = 0; i < num_of_players; i++){
+        name_of_players[i] = prompt("Enter the name of player"+ i+1);
+    }
+
+    return name_of_players;
+}
+
+
+//function drag_card_handler(e){
+//    console.log('hello');
+//    this.style.opacity = '0.5';
+//
+//}
+//
+//function card_drop_handler(event){
+//    if(event.stopPropagation){
+//        event.stopPropagation();
+//    }
+//}
+//
+//function handleDragOver(e){
+//    if(e.preventDefault){
+//        e.preventDefault();
+//    }
+//
+//    e.dataTransfer.dropEffect = 'move';
+//    return false;
+//}
+//function handleDragEnter(e){
+//    e.target.classList.add('over');
+//}
+//function handleDragLeave(e){
+//    e.target.classList.remove('over');
+//}
+//
+//function initialize_event_listeners(){
+//
+//    var cards = id('card_1');
+//    cards.addEventListener('dragstart', drag_card_handler, false);
+//    cards.addEventListener('dragenter', handleDragEnter, false);
+//    cards.addEventListener('dragover', handleDragOver, false);
+//    cards.addEventListener('dragleave', handleDragLeave, false);
+//
+//}initialize_event_listeners();
+
+
--- ui_javascripts/main_screen.js
+++ ui_javascripts/main_screen.js
@@ -3,64 +3,28 @@
 //Main Game class creates a new game
 //Also starts the game which deals the first round to each player
 function Game(){
-	this.game = new game;
-	var player_names = ['ryan','computer_1', 'computer_2', 'computer_3'];
-	this.num_of_players = 4;
-	this.player_that_ends_round = null;
-	//create the players and the deck
-	this.game_constants = _game.initialize_game(4, player_names);
-	this.game_constants = _game.new_round(game_constants);
-	
+    var player_names = get_number_of_players_and_player_names();
+    var _game = new game();
+
+    var _constants = _game.initialize_game(player_names.length, player_names);
+    var round = _game.new_round(_constants);
+    draw_game(round.round);
 }
 
-//current_round is called by a player action
-//@obj is the object that the player action happened on
-Game.prototype.current_round = function(obj){
-	var action = obj.getAttribute('id');
-	var current_player = this.game_constants.players[player_num];
-	
-	switch(action){
-		case 'draw_deck':{
-			var card = current_player.draw_from_deck_or_discard(game_constants.deck);
-			break;
-		}
-		case 'draw_discard':{
-			var card = current_player.draw_from_deck_or_discard(this.game_constants.discard_pile);
-			return current_playerplayer.hand.push(card);
-			break;
-		}case 'end_turn':{
-			if(player_num <= num_of_players ){
-				return player_num++;
-			}else{
-				return player_num = 0; //go back to first player
-			}
-			break;
-		}case 'laydown':{
-			var can_laydown = this.can_player_laydown(current_player.hand);
-			if(can_laydown.can){
-				current_player.hand = current_player.laydown(current_player.hand);
-				current_player.score = this.calculate_score(current_player.hand);
-			}else{
-				alert('You can not lay down at this time');
-			}
-			break;	
-		}case 'laydown_to_win':{
-			var can_laydown = this.can_player_laydown(current_player.hand);
-			if(can_laydown.can){
-				this.current_player.laydown_to_win(current_player.hand);
-				this.player_that_ends_round = player_num;
-				this.warn_other_players();
-			}else{
-				alert("You don't have the correct cards to laydown!");
-			}
-			break;
-		}
-		
-	}			
-}
+function draw_game(round_num){
+    var screen = document.getElementById("main_screen");
+    var deck_area = document.getElementById("deck_and_discard");
+    var player_1_area = document.getElementById("player_1_hand");
+    var player_2_area = document.getElementById("player_2_hand");
+    var player_3_area = document.getElementById("player_3_hand");
+    var player_4_area = document.getElementById("player_4_hand");
 
-Game.prototype.warn_other_players = function(){
-	
+    var _deck = document.createElement('div');
+
+    _deck.setAttribute("id", 'playing_deck');
+    _deck.setAttribute('class', "card back");
+
+    deck_area.appendChild(_deck);
 }
 
 
