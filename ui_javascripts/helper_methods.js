b3dc53718df3a12a7ae99e88b1fcab21e5125a14
Ryan Earl <ryan.earl@businesspromotion.com>
2016-02-22 19:14:18 -0700

    Broken code broken broken broken, trying to implement custom prompt.

--- ui_javascripts/helper_methods.js
+++ ui_javascripts/helper_methods.js
@@ -20,18 +20,77 @@ function get_number_of_players_and_player_names(){
     return player_names;
 }
 
-function get_number_of_bots_and_players_name(){
-    var num_of_players = prompt("How many computers would you like to play with? (between 1 - 3).");
-    var players_name = prompt("Enter your name.");
-    var players_names = [];
-    var total_players = parseInt(num_of_players, 10) + 1;
+function get_number_of_bots_and_players_name(){    
+    dialog_confirm('number', true, function(value){
+
+    });
     
-    players_names[0] = players_name;
-    for(var i = 1; i < total_players; i++){
-        players_names[i] = "Bot Man #" + i;
+    
+
+}
+
+function another_handler(value){
+        var number_of_bots = value;
+        var players_names = [];
+        var players_name = '';
+        
+        console.log(number_of_bots);
+    
+        var total_players = parseInt(number_of_bots, 10) + 1;
+    
+        players_names[0] = players_name;
+        for(var i = 1; i < total_players; i++){
+            players_names[i] = "Bot Man #" + i;
+        }
+    
+        return players_names;
+}
+
+//@type string, text or number
+//@computer, bool, true = playing against bots, false = hotseat
+function dialog_confirm(type, computer, callback){
+    var input_popup = id('input_popup');
+    var title = id('input_title');
+    var input = id('player_input');
+    var ok_button = id('dialog_ok_button');
+    var error = id('input_error');
+
+    input.value = '';
+    input_popup.style.display = 'block';
+    
+    if(type === 'text'){
+        input.setAttribute('placeholder', "Name:");
+        input.setAttribute('type', 'text');
+        if(computer){
+            title.innerText = "Please Enter Your Name:";
+        }else{
+            title.innerText = "Enter Players Name:";
+        }
+    }else{
+        input.setAttribute('type', 'number');
+        if(computer){
+            title.innerText = "Number of Computers? (between 1 - 3)";
+            input.value = 1;
+            input.setAttribute('min', '1');
+            input.setAttribute('max', '3');
+        }else{
+            title.innerText = "Number of Players? (up to 4)";
+            input.value = 2;
+            input.setAttribute('min', '2');
+            input.setAttribute('max', '4');
+        }
     }
     
-    return players_names;
+    ok_button.removeEventListener('click', handle_input_button, false);
+    ok_button.addEventListener('click', handle_input_button, false);
+    
+    function handle_input_button(event){
+        if(input.value === ''){
+            error.setAttribute('display', 'inline-block');
+        }else{
+            callback(input.value);
+        }  
+    } 
 }
 
 Array.prototype.remove_dupes = function(){
