2af82827310416eae4a8c935898a836c7b717204
Ryan Earl <ryan.earl@businesspromotion.com>
2016-04-04 18:28:44 -0600

    start on saving the game state before pause or close

--- ui_javascripts/home_screen.js
+++ ui_javascripts/home_screen.js
@@ -4,6 +4,19 @@
         document.addEventListener('DOMContentLoaded', function(){
             var attachFastClick = Origami.fastclick;
             attachFastClick(document.body);
+            window.addEventListener('beforeunload', handle_game_pause, false);
         }, false);
     }
 })();
+
+function handle_game_pause(event){
+    var confirm_message = "\o/";
+    
+    var game_state = JSON.parse(window.localStorage.getItem('game_state')) || {};
+    
+    game_state = JSON.stringify(_game);
+    window.localStorage.setItem('game_state', game_state);
+    
+    event.preventDefault();
+    return confirm_message;
+}
